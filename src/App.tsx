901d220a00d414a1fa47af0af41a1a2f7f577f84
Enea Guidi <guidienea@gmail.com>
2021-08-12 20:10:23 +0200

    Collapsed stylesheets to single global one

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,9 @@ import "@ionic/react/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 
+/* Import some global styling rules */
+import "./theme/styles.css";
+
 const App: React.FC = () => (
   <IonApp>
     <AuthProvider>
